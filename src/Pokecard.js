22d0db9abf5fcefb374c010861ada8f6de920ce6
Patrick Sontag <pmsontag84@gmail.com>
2022-09-23 09:47:14 -0500

    basic files

---
+++ src/Pokecard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const pokemonAPI = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+function Pokecard({id}) {
+
+    let pokemonImage = `${pokemonAPI}${prop.id}.png`
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Pokecard;
